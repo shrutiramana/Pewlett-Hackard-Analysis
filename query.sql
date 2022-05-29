7087228db57cca170c0d7bea6c09aa6037c82ddb
shrutiramana <98556229+shrutiramana@users.noreply.github.com>
2022-05-29 16:08:03 -0500

    adding few more files & updating query file

--- query.sql
+++ query.sql
@@ -37,3 +37,91 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 
 SELECT * FROM retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+	
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no;
+
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no;
+
+
+--(emp_no, fname, lname - retirement info) - retirement_info table.
+--(emp_no, dept_no, from_date, to_date) - dept_emp table
+
+SELECT ri.emp_no, ri.first_name , ri.last_name , de.to_date
+INTO current_emp
+FROM retirement_info as ri 
+LEFT JOIN dept_emp as de 
+ON ri.emp_no = de.emp_no
+WHERE de.to_date=('9999-01-01');
+
+
+--Employee count by department number.
+
+SELECT COUNT(ce.emp_no),de.dept_no
+INTO emp_cnt_dept
+FROM current_emp AS ce
+LEFT JOIN dept_emp AS de 
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Employee information
+
+SELECT e.emp_no , e.first_name,
+	e.last_name,e.gender,s.salary
+into emp_info
+FROM employees AS e
+LEFT JOIN salaries AS s
+ON e.emp_no = s.emp_no
+inner join dept_emp as de
+on (e.emp_no=de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+
+AND (de.to_date = '9999-01-01');
+
+
+
